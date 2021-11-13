55d9a378c86248b7a03d0f6950ab225cce94c476
Reza Shams <rezashams86@gmail.com>
2021-11-13 23:10:10 +0000

    Stablish spring boot

---
+++ src/main/java/com/hotel/roommgn/HotelAppRoomMgnApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.roommgn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAppRoomMgnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAppRoomMgnApplication.class, args);
+	}
+
+}
